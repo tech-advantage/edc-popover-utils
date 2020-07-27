3ee5d5483372bed6677d65ff9eb96da5196981f7
Nitorac <nitorac.r@gmail.com>
2020-07-27 18:30:36 +0200

    Added IPopoverOptions export

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,5 +1,6 @@
 export { PopoverItem } from './popover-item';
 export { PopoverContent } from './popover-content';
+export { IPopoverOptions } from './popover-options.interface';
 export { PopoverOptions } from './popover-options';
 export { PopoverLabels } from './popover-labels';
 export { PopoverConfig } from './popover-config';
