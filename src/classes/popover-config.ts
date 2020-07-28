6a261057363d922990ed346d1458a0e346dbb522
Daniel Mayer <daniel.mayer@tech-advantage.com>
2020-07-28 09:55:15 +0200

    Remove unnecessary icon option

--- src/classes/popover-config.ts
+++ src/classes/popover-config.ts
@@ -6,14 +6,12 @@ import { IPopoverOptions } from './popover-options.interface';
  * Configuration for the popover, passed as an input from the host element
  *
  * target: The target that will trigger the popover - can be an element id, the reference of the element itself, or classes
- * icon: The class name to use for the icon
  * content: The popover's content: the title for the header, the description, articles and links for the body
  * labels: The translated labels
  * options: Popover customizations
  */
 export class PopoverConfig {
     target: Element;
-    icon: string;
     content: PopoverContent;
     labels: PopoverLabels;
     options?: IPopoverOptions;
